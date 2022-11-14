790efa8d1420fb717b7b7e6cf841beba0191ca97
Diogo Araújo <dmaraujo.2011@gmail.com>
2022-11-14 14:06:59 -0300

    semana 12

---
+++ semana 12/frontend/atividade1/script1.js
@@ -0,0 +1,14 @@
+function cubo (n){
+  n = document.getElementById('potencia-input').value 
+  alert (n*n*n)
+}
+
+function metade(n){
+  n = document.getElementById('metade-input').value
+  alert (n/2)
+}
+
+function areaCirculo(r){
+  r = document.getElementById('area-input').value
+  alert (3.1416 * (r*r))
+}
